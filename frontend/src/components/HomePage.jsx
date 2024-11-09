11d2f51fd62d925f23543a74130453b479586778
maaladina <maaladina@yandex.ru>
2024-11-09 14:04:42 +0400

    add Channels, Modal

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -9,12 +9,11 @@ import filter from 'leo-profanity';
 import Header from './Header';
 import { setChannels } from '../slices/channelsSlice.js';
 import { setMessages } from '../slices/messagesSlice.js';
-import Channel from './Channel.jsx';
+import Channels from './Channels.jsx';
 import AddChannel from './modals/AddChannel.jsx';
 import RemoveChannel from './modals/RemoveChannel.jsx';
 import RenameChannel from './modals/RenameChannel.jsx';
 import routes from '../routes.js';
-import { showModal } from '../slices/modalSlice.js';
 import 'react-toastify/dist/ReactToastify.css';
 import showToast from './toast.js';
 
@@ -26,6 +25,19 @@ const getAuthHeader = () => {
   return {};
 };
 
+const Modal = () => {
+  const modalType = useSelector((state) => state.modal.type);
+  const item = useSelector((state) => state.modal.currentChannel);
+  if (modalType === 'adding') {
+    return <AddChannel />;
+  } if (modalType === 'removing') {
+    return <RemoveChannel item={item} />;
+  } if (modalType === 'renaming') {
+    return <RenameChannel item={item} />;
+  }
+  return null;
+};
+
 const HomePage = () => {
   const [sendFailed, setSendFailed] = useState(false);
   const channels = useSelector((state) => state.channels.channels);
@@ -33,8 +45,6 @@ const HomePage = () => {
   const activeChannel = channels.find((channel) => channel.id === activeChannelId);
   const messages = useSelector((state) => state.messages.messages);
   const username = useSelector((state) => state.auth.username);
-  const modalType = useSelector((state) => state.modal.type);
-  const item = useSelector((state) => state.modal.currentChannel);
   const activeMessages = [];
   messages.forEach((message) => {
     if (message.channelId === activeChannelId) {
@@ -44,17 +54,6 @@ const HomePage = () => {
   const dispatch = useDispatch();
   const inputRef = useRef();
   const { t } = useTranslation();
-
-  const renderModal = () => {
-    if (modalType === 'adding') {
-      return <AddChannel />;
-    } if (modalType === 'removing') {
-      return <RemoveChannel item={item} />;
-    } if (modalType === 'renaming') {
-      return <RenameChannel item={item} />;
-    }
-    return null;
-  };
 
   useEffect(() => {
     const getChannels = async () => {
@@ -68,11 +67,6 @@ const HomePage = () => {
     };
     getChannels();
   }, []);
-
-  const handleAdd = (e) => {
-    e.preventDefault();
-    dispatch(showModal({ type: 'adding', item: null }));
-  };
 
   const formik = useFormik({
     initialValues: {
@@ -105,27 +99,7 @@ const HomePage = () => {
             <Header />
             <div className="container h-100 my-4 overflow-hidden rounded shadow">
               <div className="row h-100 bg-white flex-md-row">
-                <div className="col-4 col-md-2 border-end px-0 bg-light flex-column h-100 d-flex">
-                  <div className="d-flex mt-1 justify-content-between mb-2 ps-4 pe-2 p-4">
-                    <b>{t('chat.channels')}</b>
-                    <button type="button" className="p-0 text-primary btn btn-group-vertical" onClick={handleAdd}>
-                      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="20" height="20" fill="currentColor">
-                        <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z" />
-                        <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4" />
-                      </svg>
-                      <span className="visually-hidden">+</span>
-                    </button>
-                  </div>
-                  <ul id="channels-box" className="nav flex-column nav-pills nav-fill px-2 mb-3 overflow-auto h-100 d-block">
-                    {channels.map((channel) => (
-                      <Channel
-                        channel={channel}
-                        activeChannelId={activeChannelId}
-                        key={channel.id}
-                      />
-                    ))}
-                  </ul>
-                </div>
+                <Channels />
                 <div className="col p-0 h-100">
                   <div className="d-flex flex-column h-100">
                     <div className="bg-light mb-4 p-3 shadow-sm small">
@@ -181,7 +155,7 @@ const HomePage = () => {
           </div>
         </div>
       </div>
-      {renderModal()}
+      <Modal />
     </>
   );
 };
